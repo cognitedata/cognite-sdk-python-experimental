874b63ed1c258849ca2e959dc4228c7666339a09
Sander Land <sander.land@cognite.com>
2020-06-04 11:02:14 +0200

    retries and docs

--- cognite/experimental/_client.py
+++ cognite/experimental/_client.py
@@ -3,6 +3,7 @@ from typing import Callable, Dict, Optional, Union
 
 from cognite.client._api.datapoints import DatapointsAPI
 from cognite.client._api.files import FilesAPI
+from cognite.client._api_client import APIClient
 from cognite.client._cognite_client import CogniteClient as Client
 from cognite.experimental._api.assets import ExperimentalAssetsAPI
 from cognite.experimental._api.entity_extraction import EntityExtractionAPI
@@ -28,6 +29,16 @@ class ExperimentalFilesApi(FilesAPI):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.unstructured = GrepAPI(self._config, api_version="playground", cognite_client=self)
+
+
+APIClient.RETRYABLE_POST_ENDPOINTS |= {
+    "/timeseries/synthetic/query",
+    "/files/unstructured/search",
+    "/files/unstructured/downloadlink/parsed",
+}
+APIClient.RETRYABLE_POST_ENDPOINTS |= {
+    f"/{api}/{endpoint}" for api in ["types", "labels", "relationships"] for endpoint in ["list", "byids", "search"]
+}
 
 
 class CogniteClient(Client):
