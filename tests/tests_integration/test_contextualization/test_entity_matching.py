5463b1b585b75de468e40efd73d674c5652242ae
Rebecca Wiborg Seyfarth <rebecca.wiborg.seyfarth@cognite.com>
2020-08-10 10:37:03 +0200

    Fix test

--- tests/tests_integration/test_contextualization/test_entity_matching.py
+++ tests/tests_integration/test_contextualization/test_entity_matching.py
@@ -35,6 +35,13 @@ class TestEntityMatchingIntegration:
         assert {"matches", "matchFrom"} == set(job.result["items"][0].keys())
         assert "Completed" == job.status
 
+        # Retrieve model-info
+        info = EMAPI.retrieve(model_id=model.model_id)
+        assert info.classifier == "RandomForest"
+        assert info.feature_type == "bigram"
+        assert info.keys_from_to == [["name", "bloop"]]
+        assert info.model_type == "Supervised"
+
         EMAPI.delete(model)
 
     def test_ml_fit(self):
@@ -50,13 +57,6 @@ class TestEntityMatchingIntegration:
         )
         assert isinstance(model, EntityMatchingModel)
         assert "Queued" == model.status
-
-        # Retrieve model-info
-        info = EMAPI.retrieve(model_id=model.model_id)
-        assert info.classifier == "RandomForest"
-        assert info.feature_type == "bigram"
-        assert info.keys_from_to == [["name", "bloop"]]
-        assert info.model_type == "Supervised"
 
         job = model.predict_ml(match_from=[{"name": "foo-bar"}], match_to=[{"bloop": "foo-42"}])
         assert "Completed" == model.status
