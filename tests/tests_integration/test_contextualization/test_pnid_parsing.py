eb80b71d09e284d99b4503029d38e876c83d97a2
Ching-yu Lin <ching.yulin@cognite.com>
2020-09-07 16:28:37 +0200

    integration test

--- tests/tests_integration/test_contextualization/test_pnid_parsing.py
+++ tests/tests_integration/test_contextualization/test_pnid_parsing.py
@@ -9,7 +9,9 @@ PNIDAPI = COGNITE_CLIENT.pnid_parsing
 
 class TestPNIDParsingIntegration:
     def test_run_fails(self):
-        job = PNIDAPI.parse(1, [])
+        entities = ["a", "b"]
+        file_id = 123432423
+        job = PNIDAPI.detect(file_id, entities, name_mapping={"a": "c"}, partial_match=False, min_tokens=3)
         with pytest.raises(ModelFailedException) as exc_info:
             job.result
         assert "failed with error" in str(exc_info.value)
