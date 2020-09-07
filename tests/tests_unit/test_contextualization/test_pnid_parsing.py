c8c0f2138a24abf2747cc0450148dd51d67209ca
Ching-yu Lin <ching.yulin@cognite.com>
2020-09-07 16:06:55 +0200

    unittest

--- tests/tests_unit/test_contextualization/test_pnid_parsing.py
+++ tests/tests_unit/test_contextualization/test_pnid_parsing.py
@@ -11,17 +11,6 @@ COGNITE_CLIENT = CogniteClient()
 PNIDAPI = COGNITE_CLIENT.pnid_parsing
 
 
-@pytest.fixture
-def mock_parse(rsps):
-    response_body = {"jobId": 123, "status": "Queued"}
-    rsps.add(
-        rsps.POST,
-        PNIDAPI._get_base_url_with_base_path() + PNIDAPI._RESOURCE_PATH + "/parse",
-        status=200,
-        json=response_body,
-    )
-    yield rsps
-
 
 @pytest.fixture
 def mock_detect(rsps):
@@ -84,33 +73,6 @@ def mock_status_failed(rsps):
 
 
 class TestPNIDParsing:
-    def test_parse(self, mock_parse, mock_status_ok):
-        entities = ["a", "b"]
-        file_id = 123432423
-        job = PNIDAPI.parse(file_id, entities, name_mapping={"a": "c"}, partial_match=False)
-        assert isinstance(job, ContextualizationJob)
-        assert "Queued" == job.status
-        assert {"svgUrl": "x"} == job.result
-        assert "Completed" == job.status
-        assert 123 == job.job_id
-
-        n_parse_calls = 0
-        n_status_calls = 0
-        for call in mock_parse.calls:
-            if "parse" in call.request.url:
-                n_parse_calls += 1
-                assert {
-                    "entities": entities,
-                    "fileId": file_id,
-                    "nameMapping": {"a": "c"},
-                    "partialMatch": False,
-                    "minTokens": 2,
-                } == jsgz_load(call.request.body)
-            else:
-                n_status_calls += 1
-                assert "/123" in call.request.url
-        assert 1 == n_parse_calls
-        assert 1 == n_status_calls
 
     def test_detect(self, mock_detect, mock_status_ok):
         entities = ["a", "b"]
