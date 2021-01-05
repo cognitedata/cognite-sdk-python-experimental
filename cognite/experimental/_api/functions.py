168ada83daea2f54b813d3ab8cd35e6d42aaa4f6
Matz Haugen <matz.haugen@cognite.com>
2021-01-05 13:19:36 +0100

    change docs

--- cognite/experimental/_api/functions.py
+++ cognite/experimental/_api/functions.py
@@ -437,7 +437,7 @@ class FunctionCallsAPI(APIClient):
     def retrieve(
         self, call_id: int, function_id: Optional[int] = None, function_external_id: Optional[str] = None
     ) -> FunctionCall:
-        """`Retrieve call by id. <https://docs.cognite.com/api/playground/#operation/get-api-playground-projects-project-functions-function_name-calls-call_id>`_
+        """`Retrieve call by id. <https://docs.cognite.com/api/playground/#operation/byidsFunctionSchedules>`_
 
         Args:
             call_id (int): ID of the call.
