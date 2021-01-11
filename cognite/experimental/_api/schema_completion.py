23ac397adeb7299fb5d5b684a227ae8065e9568c
Sander Land <sander.land@cognite.com>
2021-01-11 12:26:08 +0100

    domain completion

--- cognite/experimental/_api/schema_completion.py
+++ cognite/experimental/_api/schema_completion.py
@@ -6,7 +6,7 @@ from cognite.experimental._context_client import ContextAPI
 
 
 class SchemaCompletionAPI(ContextAPI):
-    _RESOURCE_PATH = "/schemas"
+    _RESOURCE_PATH = "/context/schemas"
 
     def complete_type(self, external_id: str) -> ContextualizationJob:
         """Completes a schema uploaded in CDF as a type.
@@ -16,17 +16,25 @@ class SchemaCompletionAPI(ContextAPI):
 
         Returns:
             ContextualizationJob: Resulting queued job. Note that .results property of this job will block waiting for results."""
-        return self._run_job(job_path="/completetype", status_path="/", external_id=external_id)
+        return self._run_job(job_path="/type", status_path="/", external_id=external_id)
 
-   def complete_domain(self, external_id: str, template_name: str, asset_property: str, version: int = None ) -> ContextualizationJob:
+    def complete_domain(
+        self, external_id: str, template_name: str, asset_property: str, version: int = None
+    ) -> ContextualizationJob:
         """Completes a schema uploaded in CDF as a domain.
 
         Args:
             external_id (str), version (int): External ID of the domain to work on, with optional version.
             template_name: Name of the template to be completed within the domain
-            asset_property: Which (constant) field in the template defines the externalId of the parent asset in each entry.
+            asset_property: Which field (with constant type) in the template defines the externalId of the parent asset in each entry.
 
         Returns:
             ContextualizationJob: Resulting queued job. Note that .results property of this job will block waiting for results."""
-        return self._run_job(job_path="/completedomain", status_path="/", external_id=external_id,template_name=template_name,asset_property=asset_property,version=version)
-
+        return self._run_job(
+            job_path="/domain",
+            status_path="/",
+            external_id=external_id,
+            template_name=template_name,
+            asset_property=asset_property,
+            version=version,
+        )
