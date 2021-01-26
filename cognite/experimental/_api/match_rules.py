f083601332a3b4a5780e480735106f253b78afa2
ola <ola.liabotro@cognite.com>
2021-01-26 17:20:03 +0100

    use a special job class for suggest and apply that blocks on .rules, also use list and rules class form pipelines

--- cognite/experimental/_api/match_rules.py
+++ cognite/experimental/_api/match_rules.py
@@ -3,15 +3,15 @@ from typing import Dict, List, Union
 from cognite.client.data_classes import ContextualizationJob
 
 from cognite.experimental._context_client import ContextAPI
-from cognite.experimental.data_classes import MatchRule
+from cognite.experimental.data_classes import EntityMatchingMatchRuleList, MatchRulesApplyJob, MatchRulesSuggestJob
 
 
 class MatchRulesAPI(ContextAPI):
     _RESOURCE_PATH = "/context/matchrules"
 
     def apply(
-        self, sources: List[dict], targets: List[dict], rules: List[Union[dict, MatchRule]],
-    ) -> ContextualizationJob:
+        self, sources: List[dict], targets: List[dict], rules: Union[List[dict], EntityMatchingMatchRuleList],
+    ) -> MatchRulesApplyJob:
         """Apply match rules with priorities to source entities with target entities.
 
         Args:
@@ -21,10 +21,21 @@ class MatchRulesAPI(ContextAPI):
         Returns:
             ContextualizationJob: Resulting completed job. Note that this function waits for completion."""
 
-        job = self._run_job(job_path="/apply", status_path="/apply/", sources=sources, targets=targets, rules=rules,)
-        return job
+        if isinstance(rules, EntityMatchingMatchRuleList):
+            rules = [
+                {k: r.dump()[k] for k in ["extractors", "conditions", "priority"] if r.dump().get(k)} for r in rules
+            ]
 
-    def suggest(self, sources: List[dict], targets: List[dict], matches: List[dict],) -> ContextualizationJob:
+        return self._run_job(
+            job_path="/apply",
+            status_path="/apply/",
+            sources=sources,
+            targets=targets,
+            rules=rules,
+            job_cls=MatchRulesApplyJob,
+        )
+
+    def suggest(self, sources: List[dict], targets: List[dict], matches: List[dict],) -> MatchRulesSuggestJob:
         """Extract tags from P&ID based on pattern
 
         Args:
@@ -36,5 +47,10 @@ class MatchRulesAPI(ContextAPI):
             ContextualizationJob: Resulting queued job. Note that .results property of this job will block waiting for results."""
 
         return self._run_job(
-            job_path="/suggest", status_path="/suggest/", sources=sources, targets=targets, matches=matches,
+            job_path="/suggest",
+            status_path="/suggest/",
+            sources=sources,
+            targets=targets,
+            matches=matches,
+            job_cls=MatchRulesSuggestJob,
         )
