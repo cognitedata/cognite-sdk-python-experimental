0d710d9ea9d2da3d7f529efbaa7054e152266ac7
Ching-yu Lin <ching.yulin@cognite.com>
2021-10-04 12:36:59 +0200

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import pytest
 import responses
@@ -7,6 +8,13 @@ import responses
 @pytest.fixture
 def rsps():
     with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.POST,
+            re.compile("https://login.microsoftonline.com.*"),
+            status=200,
+            json={"token_type": "Bearer", "expires_in": 3599, "ext_expires_in": 3599, "access_token": "a.b.c"},
+        )
+        rsps.assert_all_requests_are_fired = False
         yield rsps
 
 
