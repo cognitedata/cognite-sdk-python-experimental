ad758cad3eacf86845aa98985ad6aa2c90a6cebc
Ching-yu Lin <ching.yulin@cognite.com>
2021-10-06 09:24:18 +0200

    unit test

--- tests/tests_unit/test_contextualization/test_template_completion.py
+++ tests/tests_unit/test_contextualization/test_template_completion.py
@@ -2,7 +2,6 @@ import re
 
 import pytest
 from cognite.client.data_classes import ContextualizationJob
-from cognite.client.exceptions import ModelFailedException
 
 from cognite.experimental import CogniteClient
 from tests.utils import jsgz_load
@@ -36,11 +35,35 @@ def mock_complete_template(rsps):
 
 
 @pytest.fixture
+def mock_suggest_instance(rsps):
+    response_body = {"jobId": 123, "status": "Queued"}
+    rsps.add(
+        rsps.POST,
+        SCAPI._get_base_url_with_base_path() + SCAPI._RESOURCE_PATH + "/instancesuggestion",
+        status=200,
+        json=response_body,
+    )
+    yield rsps
+
+
+@pytest.fixture
 def mock_status_ok(rsps):
     response_body = {"jobId": 123, "status": "Completed", "items": []}
     rsps.add(
         rsps.GET,
         re.compile(SCAPI._get_base_url_with_base_path() + SCAPI._RESOURCE_PATH + "/\\d+"),
+        status=200,
+        json=response_body,
+    )
+    yield rsps
+
+
+@pytest.fixture
+def mock_suggest_instance_status_ok(rsps):
+    response_body = {"jobId": 123, "status": "Completed", "items": []}
+    rsps.add(
+        rsps.GET,
+        re.compile(SCAPI._get_base_url_with_base_path() + SCAPI._RESOURCE_PATH + "/instancesuggestion/\\d+"),
         status=200,
         json=response_body,
     )
@@ -82,6 +105,24 @@ class TestSchemaCompletion:
         extract_calls = 0
         n_status_calls = 0
         for call in mock_complete_template.calls:
+            if call.request.method == "POST":
+                extract_calls += 1
+                assert {"externalId": eid, "templateName": template} == jsgz_load(call.request.body)
+            else:
+                n_status_calls += 1
+                assert "/123" in call.request.url
+        assert 1 == extract_calls
+        assert 1 == n_status_calls
+
+    def test_suggest_instance(self, mock_suggest_instance, mock_suggest_instance_status_ok):
+        eid = "schematocomplete"
+        template = "templatename"
+        job = SCAPI.suggest_instance(external_id=eid, template_name=template)
+        assert "Queued" == job.status
+        assert {"items": []} == job.result
+        extract_calls = 0
+        n_status_calls = 0
+        for call in mock_suggest_instance.calls:
             if call.request.method == "POST":
                 extract_calls += 1
                 assert {"externalId": eid, "templateName": template} == jsgz_load(call.request.body)
