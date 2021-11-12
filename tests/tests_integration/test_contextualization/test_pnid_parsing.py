383fe94f6326c77b5bd3d7a716f805f29870f0e5
Emel Varol <emel.varol@cognite.com>
2021-11-12 13:14:44 +0100

    Skip some context tests

--- tests/tests_integration/test_contextualization/test_pnid_parsing.py
+++ tests/tests_integration/test_contextualization/test_pnid_parsing.py
@@ -10,6 +10,7 @@ PNID_FILE_ID = 3261066797848581
 
 
 class TestPNIDParsingIntegration:
+    @pytest.mark.skip("broken")
     def test_run_detect_str(self):
         entities = ["YT-96122", "XE-96125"]
         file_id = PNID_FILE_ID
@@ -36,6 +37,7 @@ class TestPNIDParsingIntegration:
         assert isinstance(ocr_result[0], PNIDDetectionList)
         assert isinstance(ocr_result[0]._repr_html_(), str)
 
+    @pytest.mark.skip("broken")
     def test_run_convert(self):
         items = [
             {
