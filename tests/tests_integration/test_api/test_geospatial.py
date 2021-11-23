40b9bc89f67d470e9d8a2b454db6225fb404abaf
Pawel <pawel.zwolenski@cognite.com>
2021-11-23 11:33:11 +0100

    fix import

--- tests/tests_integration/test_api/test_geospatial.py
+++ tests/tests_integration/test_api/test_geospatial.py
@@ -15,7 +15,7 @@ from cognite.experimental.data_classes.geospatial import (
     FeatureList,
     FeatureType,
     FeatureTypeUpdate,
-    OrderSpec
+    OrderSpec,
 )
 
 COGNITE_CLIENT = CogniteClient(max_workers=1)
