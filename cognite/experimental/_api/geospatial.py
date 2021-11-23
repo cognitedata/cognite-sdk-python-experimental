789acc9f3e4c220484e0b387a22bf58c4bad195d
Pawel <pawel.zwolenski@cognite.com>
2021-11-23 06:14:04 +0100

    fix typo

--- cognite/experimental/_api/geospatial.py
+++ cognite/experimental/_api/geospatial.py
@@ -322,7 +322,7 @@ class ExperimentalGeospatialAPI(APIClient):
         resource_path = resource_path
         res = self._post(
             url_path=resource_path,
-            json={"filter": filter, "limit": limit, "output": {"attributes": attributes}, "orderBy": oderBy},
+            json={"filter": filter, "limit": limit, "output": {"attributes": attributes}, "orderBy": orderBy},
         )
         return cls._load(res.json()["items"], cognite_client=self._cognite_client)
 
