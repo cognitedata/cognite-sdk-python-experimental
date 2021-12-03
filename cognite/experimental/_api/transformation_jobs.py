a3a8ad5e2fc28dcd65bf36cf6bf3ca5b768d5432
silvavelosa <jaime.silva@cognite.com>
2021-12-03 12:42:34 +0100

    fix tests

--- cognite/experimental/_api/transformation_jobs.py
+++ cognite/experimental/_api/transformation_jobs.py
@@ -65,7 +65,7 @@ class TransformationJobsAPI(APIClient):
                 >>> c = CogniteClient()
                 >>> res = c.transformations.jobs.retrieve(id=1)
         """
-        return self._retrieve(id=id)
+        return self._retrieve_multiple(ids=id, wrap_ids=True)
 
     def list_metrics(self, id: int) -> TransformationJobMetricList:
         """`List the metrics of a single transformation job. <https://docs.cognite.com/api/playground/#operation/getTransformationJob>`_
