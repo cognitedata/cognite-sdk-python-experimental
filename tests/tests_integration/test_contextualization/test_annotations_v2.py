d55eb0fc86f9f476f02d9ae3ec281a8aa2da5702
Peet Cremer <peet.cremer@cognite.com>
2022-01-19 16:10:47 +0100

    Refactor annotation_v2 integration tests cleanup

--- tests/tests_integration/test_contextualization/test_annotations_v2.py
+++ tests/tests_integration/test_contextualization/test_annotations_v2.py
@@ -2,49 +2,48 @@ from copy import deepcopy
 from typing import List, Optional
 
 import pytest
+from cognite.client.data_classes import FileMetadata
 from cognite.client.exceptions import CogniteAPIError
 
 from cognite.experimental import CogniteClient
 from cognite.experimental.data_classes import AnnotationV2, AnnotationV2Filter, AnnotationV2List, AnnotationV2Update
 
-COGNITE_CLIENT = CogniteClient()
-ANNOTATIONSAPI = COGNITE_CLIENT.annotations_v2
 
-
-def delete_with_check(delete_ids: List[int], check_ids: Optional[List[int]] = None) -> None:
+def delete_with_check(
+    cognite_client: CogniteClient, delete_ids: List[int], check_ids: Optional[List[int]] = None
+) -> None:
     if check_ids is None:
         check_ids = delete_ids
-    ANNOTATIONSAPI.delete(id=delete_ids)
+    cognite_client.annotations_v2.delete(id=delete_ids)
     try:
-        ANNOTATIONSAPI.retrieve_multiple(check_ids)
+        cognite_client.annotations_v2.retrieve_multiple(check_ids)
         raise ValueError(f"retrieve_multiple after delete successful for ids {check_ids}")
     except CogniteAPIError as e:
-        assert "Some annotation(s) not found" in str(e)
         assert e.code == 404
         missing = [i["id"] for i in e.missing]
         assert sorted(check_ids) == sorted(missing)
 
 
-def cleanup(check_ids: List[int] = []) -> None:
-    """ Cleanup all annotations with creating_app=='UnitTest' """
-    file_ids = [f.id for f in COGNITE_CLIENT.files.list(limit=100) if f.id]
-    filter = AnnotationV2Filter(
-        annotated_resource_type="file",
-        annotated_resource_ids=[{"id": fid} for fid in file_ids],
-        creating_app="UnitTest",
-    )
-    annotation_ids = [a.id for a in ANNOTATIONSAPI.list(filter=filter)]
-    if annotation_ids:
-        delete_with_check(annotation_ids, check_ids)
+@pytest.fixture(scope="class")
+def cognite_client() -> CogniteClient:
+    return CogniteClient()
 
 
 @pytest.fixture
-def file_id() -> int:
-    files = COGNITE_CLIENT.files.list(limit=100)
-    for file in files:
-        if file.id:
-            return file.id
-    raise ValueError("No file found")
+def file_id(cognite_client: CogniteClient) -> int:
+    # Create a test file
+    name = "annotation_v2_unit_test_file"
+    file = cognite_client.files.create(FileMetadata(external_id=name, name=name), overwrite=True)[0]
+    yield file.id
+    # Teardown all annotations to the file
+    filter = AnnotationV2Filter(
+        annotated_resource_type="file", annotated_resource_ids=[{"id": file.id}], creating_app="UnitTest",
+    )
+    annotation_ids = [a.id for a in cognite_client.annotations_v2.list(filter=filter)]
+    if annotation_ids:
+        delete_with_check(cognite_client, annotation_ids)
+    # Teardown the file itself
+    cognite_client.files.delete(id=file.id)
 
 
 @pytest.fixture
@@ -65,17 +64,18 @@ def base_annotation2(base_annotation: AnnotationV2):
 def check_created_vs_base(base_annotation: AnnotationV2, created_annotation: AnnotationV2) -> None:
     base_dump = base_annotation.dump()
     created_dump = created_annotation.dump()
+    special_keys = ["id", "created_time", "last_updated_time"]
     found_special_keys = 0
     for k, v in created_dump.items():
-        if k in ["id", "created_time", "last_updated_time"]:
+        if k in special_keys:
             found_special_keys += 1
             assert v is not None
         else:
             assert v == base_dump[k]
-    assert found_special_keys == 3
+    assert found_special_keys == len(special_keys)
 
 
-def _test_list_on_created_annotations(annotations: AnnotationV2List, limit: int = 25):
+def _test_list_on_created_annotations(cognite_client: CogniteClient, annotations: AnnotationV2List, limit: int = 25):
     annotation = annotations[0]
     filter = AnnotationV2Filter(
         annotated_resource_type=annotation.annotated_resource_type,
@@ -85,7 +85,7 @@ def _test_list_on_created_annotations(annotations: AnnotationV2List, limit: int 
         creating_app_version=annotation.creating_app_version,
         creating_user=annotation.creating_user,
     )
-    annotations_list = ANNOTATIONSAPI.list(filter=filter, limit=limit)
+    annotations_list = cognite_client.annotations_v2.list(filter=filter, limit=limit)
     assert isinstance(annotations_list, AnnotationV2List)
     if limit == -1 or limit > len(annotations):
         assert len(annotations_list) == len(annotations)
@@ -107,56 +107,48 @@ def _test_list_on_created_annotations(annotations: AnnotationV2List, limit: int 
 
 
 class TestAnnotationsV2Integration:
-    def test_create_single_annotation(self, base_annotation: AnnotationV2) -> None:
-        created_annotation = ANNOTATIONSAPI.create(base_annotation)
-        try:
-            assert isinstance(created_annotation, AnnotationV2)
-            check_created_vs_base(base_annotation, created_annotation)
-            assert created_annotation.creating_user == None
-        finally:
-            delete_with_check([created_annotation.id])
+    def test_create_single_annotation(self, cognite_client: CogniteClient, base_annotation: AnnotationV2) -> None:
+        created_annotation = cognite_client.annotations_v2.create(base_annotation)
+        assert isinstance(created_annotation, AnnotationV2)
+        check_created_vs_base(base_annotation, created_annotation)
+        assert created_annotation.creating_user == None
 
-    def test_create_single_annotation2(self, base_annotation2: AnnotationV2) -> None:
-        created_annotation = ANNOTATIONSAPI.create(base_annotation2)
-        try:
-            assert isinstance(created_annotation, AnnotationV2)
-            check_created_vs_base(base_annotation2, created_annotation)
-            assert created_annotation.creating_user == "unit.test@cognite.com"
-        finally:
-            delete_with_check([created_annotation.id])
+    def test_create_single_annotation2(self, cognite_client: CogniteClient, base_annotation2: AnnotationV2) -> None:
+        created_annotation = cognite_client.annotations_v2.create(base_annotation2)
+        assert isinstance(created_annotation, AnnotationV2)
+        check_created_vs_base(base_annotation2, created_annotation)
+        assert created_annotation.creating_user == "unit.test@cognite.com"
 
-    def test_create_annotations(self, base_annotation: AnnotationV2) -> None:
-        created_annotations = ANNOTATIONSAPI.create([base_annotation] * 30)
-        try:
-            assert isinstance(created_annotations, AnnotationV2List)
-            for a in created_annotations:
-                check_created_vs_base(base_annotation, a)
-        finally:
-            delete_with_check([a.id for a in created_annotations])
+    def test_create_annotations(self, cognite_client: CogniteClient, base_annotation: AnnotationV2) -> None:
+        created_annotations = cognite_client.annotations_v2.create([base_annotation] * 30)
+        assert isinstance(created_annotations, AnnotationV2List)
+        for a in created_annotations:
+            check_created_vs_base(base_annotation, a)
 
-    def test_delete_annotations(self, base_annotation: AnnotationV2) -> None:
-        created_annotations = ANNOTATIONSAPI.create([base_annotation] * 30)
-        delete_with_check([a.id for a in created_annotations])
+    def test_delete_annotations(self, cognite_client: CogniteClient, base_annotation: AnnotationV2) -> None:
+        created_annotations = cognite_client.annotations_v2.create([base_annotation] * 30)
+        delete_with_check(cognite_client, [a.id for a in created_annotations])
 
-    def test_update_annotation_by_annotation(self, base_annotation: AnnotationV2) -> None:
-        created_annotation = ANNOTATIONSAPI.create(base_annotation)
-        try:
-            updated = deepcopy(created_annotation)
-            updated.linked_resource_type = "asset"
-            updated.linked_resource_id = 1
-            updated = ANNOTATIONSAPI.update(created_annotation)
-            assert isinstance(updated, AnnotationV2)
-            updated_dump = updated.dump()
-            created_annotation_dump = created_annotation.dump()
-            for k, v in updated_dump.items():
-                if k == "last_updated_time":
-                    assert v > created_annotation_dump[k]
-                else:
-                    assert v == created_annotation_dump[k]
-        finally:
-            delete_with_check([created_annotation.id])
+    def test_update_annotation_by_annotation(
+        self, cognite_client: CogniteClient, base_annotation: AnnotationV2
+    ) -> None:
+        created_annotation = cognite_client.annotations_v2.create(base_annotation)
+        updated = deepcopy(created_annotation)
+        updated.linked_resource_type = "asset"
+        updated.linked_resource_id = 1
+        updated = cognite_client.annotations_v2.update(created_annotation)
+        assert isinstance(updated, AnnotationV2)
+        updated_dump = updated.dump()
+        created_annotation_dump = created_annotation.dump()
+        for k, v in updated_dump.items():
+            if k == "last_updated_time":
+                assert v > created_annotation_dump[k]
+            else:
+                assert v == created_annotation_dump[k]
 
-    def test_update_annotation_by_annotation_update(self, base_annotation: AnnotationV2) -> None:
+    def test_update_annotation_by_annotation_update(
+        self, cognite_client: CogniteClient, base_annotation: AnnotationV2
+    ) -> None:
         update = {
             "data": {
                 "pageNumber": 1,
@@ -170,63 +162,49 @@ class TestAnnotationsV2Integration:
             "linked_resource_id": 1,
             "linked_resource_external_id": None,
         }
-        created_annotation = ANNOTATIONSAPI.create(base_annotation)
-        try:
-            annotation_update = AnnotationV2Update(id=created_annotation.id)
-            for k, v in update.items():
-                getattr(annotation_update, k).set(v)
+        created_annotation = cognite_client.annotations_v2.create(base_annotation)
 
-            updated = ANNOTATIONSAPI.update([annotation_update])
-            assert isinstance(updated, AnnotationV2List)
-            updated = updated[0]
-            for k, v in update.items():
-                assert getattr(updated, k) == v
-        finally:
-            delete_with_check([created_annotation.id])
+        annotation_update = AnnotationV2Update(id=created_annotation.id)
+        for k, v in update.items():
+            getattr(annotation_update, k).set(v)
 
-    def test_list(self, base_annotation: AnnotationV2, base_annotation2: AnnotationV2) -> None:
-        cleanup()
-        created_annotations = ANNOTATIONSAPI.create([base_annotation] * 30 + [base_annotation2] * 30)
+        updated = cognite_client.annotations_v2.update([annotation_update])
+        assert isinstance(updated, AnnotationV2List)
+        updated = updated[0]
+        for k, v in update.items():
+            assert getattr(updated, k) == v
+
+    def test_list(
+        self, cognite_client: CogniteClient, base_annotation: AnnotationV2, base_annotation2: AnnotationV2
+    ) -> None:
+        created_annotations = cognite_client.annotations_v2.create([base_annotation] * 30 + [base_annotation2] * 30)
         first_batch = created_annotations[:30]
         second_batch = created_annotations[30:]
-        try:
-            _test_list_on_created_annotations(first_batch, limit=-1)
-            _test_list_on_created_annotations(second_batch, limit=-1)
-        finally:
-            delete_with_check([a.id for a in created_annotations])
+        _test_list_on_created_annotations(cognite_client, first_batch, limit=-1)
+        _test_list_on_created_annotations(cognite_client, second_batch, limit=-1)
 
-    def test_list_limit(self, base_annotation: AnnotationV2) -> None:
-        cleanup()
-        created_annotations = ANNOTATIONSAPI.create([base_annotation] * 30)
-        try:
-            _test_list_on_created_annotations(created_annotations, limit=5)
-            _test_list_on_created_annotations(created_annotations)
-            _test_list_on_created_annotations(created_annotations, limit=30)
-            _test_list_on_created_annotations(created_annotations, limit=-1)
-        finally:
-            delete_with_check([a.id for a in created_annotations])
+    def test_list_limit(self, cognite_client: CogniteClient, base_annotation: AnnotationV2) -> None:
+        created_annotations = cognite_client.annotations_v2.create([base_annotation] * 30)
+        _test_list_on_created_annotations(cognite_client, created_annotations, limit=5)
+        _test_list_on_created_annotations(cognite_client, created_annotations)
+        _test_list_on_created_annotations(cognite_client, created_annotations, limit=30)
+        _test_list_on_created_annotations(cognite_client, created_annotations, limit=-1)
 
-    def test_retrieve(self, base_annotation: AnnotationV2) -> None:
-        created_annotation = ANNOTATIONSAPI.create(base_annotation)
-        try:
-            retrieved_annotation = ANNOTATIONSAPI.retrieve(created_annotation.id)
-            assert isinstance(retrieved_annotation, AnnotationV2)
-            assert created_annotation.dump() == retrieved_annotation.dump()
-        finally:
-            delete_with_check([created_annotation.id])
+    def test_retrieve(self, cognite_client: CogniteClient, base_annotation: AnnotationV2) -> None:
+        created_annotation = cognite_client.annotations_v2.create(base_annotation)
+        retrieved_annotation = cognite_client.annotations_v2.retrieve(created_annotation.id)
+        assert isinstance(retrieved_annotation, AnnotationV2)
+        assert created_annotation.dump() == retrieved_annotation.dump()
 
-    def test_retrieve_multiple(self, base_annotation: AnnotationV2List) -> None:
-        created_annotations = ANNOTATIONSAPI.create([base_annotation] * 30)
-        try:
-            ids = [c.id for c in created_annotations]
-            retrieved_annotations = ANNOTATIONSAPI.retrieve_multiple(ids)
-            assert isinstance(retrieved_annotations, AnnotationV2List)
+    def test_retrieve_multiple(self, cognite_client: CogniteClient, base_annotation: AnnotationV2List) -> None:
+        created_annotations = cognite_client.annotations_v2.create([base_annotation] * 30)
+        ids = [c.id for c in created_annotations]
+        retrieved_annotations = cognite_client.annotations_v2.retrieve_multiple(ids)
+        assert isinstance(retrieved_annotations, AnnotationV2List)
 
-            # TODO assert the order and do without sorting
-            # as soon as the API is fixed
-            for ret, new in zip(
-                sorted(retrieved_annotations, key=lambda a: a.id), sorted(created_annotations, key=lambda a: a.id)
-            ):
-                assert ret.dump() == new.dump()
-        finally:
-            delete_with_check([a.id for a in created_annotations])
+        # TODO assert the order and do without sorting
+        # as soon as the API is fixed
+        for ret, new in zip(
+            sorted(retrieved_annotations, key=lambda a: a.id), sorted(created_annotations, key=lambda a: a.id)
+        ):
+            assert ret.dump() == new.dump()
