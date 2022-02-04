a7d4a3a58990cf3d81bd7310d491fd6a5946c6b6
Anders Øen Fylling <anders@nordic.email>
2022-02-04 07:29:35 +0100

    add vision endpoints for asset detection

--- cognite/experimental/utils.py
+++ cognite/experimental/utils.py
@@ -1,5 +1,7 @@
 from functools import wraps
 
+from cognite.client.utils._auxiliary import to_camel_case, to_snake_case
+
 
 def use_v1_instead_of_playground(f):
     @wraps(f)
@@ -11,3 +13,21 @@ def use_v1_instead_of_playground(f):
         return result
 
     return wrapper
+
+
+def resource_to_camel_case(resource):
+    if isinstance(resource, list):
+        return [resource_to_camel_case(element) for element in resource]
+    elif isinstance(resource, dict):
+        return {to_camel_case(k): resource_to_camel_case(v) for k, v in resource.items() if v is not None}
+    else:
+        return resource
+
+
+def resource_to_snake_case(resource):
+    if isinstance(resource, list):
+        return [resource_to_snake_case(element) for element in resource]
+    elif isinstance(resource, dict):
+        return {to_snake_case(k): resource_to_snake_case(v) for k, v in resource.items() if v is not None}
+    else:
+        return resource
