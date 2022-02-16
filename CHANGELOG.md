6191a2475ff8f1d5e6012a8cb02e9e5e1e8c2c35
Henrik Mathias Eiding <51706349+HMEiding@users.noreply.github.com>
2022-02-16 16:52:04 +0100

    Fix typehint in FunctionsAPI.create() & bump to version 0.68.2 (#284)
    * Correct type & better docs
    
    * Bump version & update CHANGELOG.md
    
    * Remove outdated documentation
    
    * Update cognite/experimental/data_classes/functions.py
    
    Co-authored-by: Håkon V. Treider <haakonvt@gmail.com>
    
    * Update cognite/experimental/_api/functions.py
    
    Co-authored-by: Håkon V. Treider <haakonvt@gmail.com>
    
    Co-authored-by: Håkon V. Treider <haakonvt@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,12 @@ Changes are grouped as follows
 - `Fixed` for any bug fixes.
 - `Security` in case of vulnerabilities.
 
+## [0.68.2]
+
+### Changed
+
+- Fixed typehint for argument `runtime` in `FunctionsAPI.create().`
+
 ## [0.68.1]
 
 ### Added
--- cognite/experimental/_api/functions.py
+++ cognite/experimental/_api/functions.py
@@ -53,7 +53,7 @@ class FunctionsAPI(APIClient):
         env_vars: Optional[Dict] = None,
         cpu: Optional[Number] = None,
         memory: Optional[Number] = None,
-        runtime: Optional[Number] = None,
+        runtime: Optional[str] = None,
     ) -> Function:
         """`When creating a function, <https://docs.cognite.com/api/playground/#operation/post-api-playground-projects-project-functions>`_
         the source code can be specified in one of three ways:\n
@@ -66,8 +66,6 @@ class FunctionsAPI(APIClient):
         - If the user gives an `api_key` when creating the function, a pre instantiated CogniteClient is passed through the `client` argument.\n
         - If the user gives one ore more secrets when creating the function, these are passed through the `secrets` argument. The API key can be access through `secrets["apikey"]`.\n
         - Data about the function call can be accessed via the argument `function_call_info`, which is a dictionary with keys `function_id` and, if the call is scheduled, `schedule_id` and `scheduled_time`.\n
-
-        The Python runtime version is 3.8.3.
 
         Args:
             name (str):                             The name of the function.
@@ -83,7 +81,7 @@ class FunctionsAPI(APIClient):
             env_vars (Dict[str, str]):              Environment variables as key/value pairs. Keys can contain only letters, numbers or the underscore character. You can create at most 100 environment variables.
             cpu (Number, optional):                 Number of CPU cores per function. Allowed values are in the range [0.1, 0.6], and None translates to the API default which is 0.25 in GCP. The argument is unavailable in Azure.
             memory (Number, optional):              Memory per function measured in GB. Allowed values are in the range [0.1, 2.5], and None translates to the API default which is 1 GB in GCP. The argument is unavailable in Azure.
-            runtime (str, optional):                The function runtime. Valid values are ["py37", "py38", "py39", `None`], and `None` translates to the API default.
+            runtime (str, optional):                The function runtime. Valid values are ["py37", "py38", "py39", `None`], and `None` translates to the API default which currently is "py38". The runtime "py3x" resolves to the latest version of the Python 3.x.y series.
 
         Returns:
             Function: The created function.
