ff97cc42efe434b65cbffbf18183eb57ecc299a1
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-06-29 16:52:04 +0200

    Added validation of requirements in folder upload

--- cognite/experimental/_constants.py
+++ cognite/experimental/_constants.py
@@ -7,3 +7,4 @@ HANDLER_FILE_NAME = "handler.py"
 MAX_RETRIES = 5
 REQUIREMENTS_FILE_NAME = "requirements.txt"
 REQUIREMENTS_REG = re.compile(r"(\[\/?requirements\]){1}$", flags=re.M)  # Matches [requirements] and [/requirements]
+UNCOMMENTED_LINE_REG = re.compile(r"^[^\#]]*.*")
