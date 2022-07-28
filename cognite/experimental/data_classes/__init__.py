db700df0fe1ae3fd7e76fd9f9783b17080e3ecbf
Ivan Polomani <ivan.polomanyi@cognite.com>
2022-07-28 14:05:15 +0200

    Properly expose alerts data_classes (#326)

--- cognite/experimental/data_classes/__init__.py
+++ cognite/experimental/data_classes/__init__.py
@@ -1,3 +1,4 @@
+from cognite.experimental.data_classes.alerts import *
 from cognite.experimental.data_classes.annotations import *
 from cognite.experimental.data_classes.contextualization import *
 from cognite.experimental.data_classes.extractionpipelines import *
