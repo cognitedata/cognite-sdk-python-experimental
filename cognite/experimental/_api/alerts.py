ae680cf6a234a83749c77af146cb40f0ac55310c
Ivan Polomani <ivano.polomani@gmail.com>
2022-07-28 10:36:37 +0200

    Alerts API alpha version (#323)

---
+++ cognite/experimental/_api/alerts.py
@@ -0,0 +1,244 @@
+from typing import Dict, List, Union
+
+from cognite.client._api_client import APIClient
+from cognite.client.utils._auxiliary import assert_type
+
+from cognite.experimental.data_classes.alerts import (
+    Alert,
+    AlertChannel,
+    AlertChannelFilter,
+    AlertChannelList,
+    AlertChannelUpdate,
+    AlertFilter,
+    AlertList,
+    AlertSubscriber,
+    AlertSubscriberList,
+    AlertSubscription,
+    AlertSubscriptionDelete,
+    AlertSubscriptionList,
+)
+
+
+class AlertChannelsAPI(APIClient):
+    _RESOURCE_PATH = "/alerts/channels"
+    _LIST_CLASS = AlertChannelList
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def create(
+        self,
+        channels: Union[AlertChannel, List[AlertChannel]],
+    ) -> Union[AlertChannel, AlertChannelList]:
+        """Create channels
+
+        Args:
+            channels (Union[AlertChannel, List[AlertChannel]]): channel(s) to create
+
+        Returns:
+            Union[AlertChannel, AlertChannelList]: created channel(s)
+        """
+        assert_type(channels, "channels", [AlertChannel, list])
+        return self._create_multiple(
+            items=channels,
+            resource_path=self._RESOURCE_PATH,
+        )
+
+    def list(
+        self,
+        external_ids: List[str] = None,
+        ids: List[int] = None,
+        parent_ids: List[str] = None,
+        metadata: Dict[str, str] = None,
+        limit=100,
+    ) -> AlertChannelList:
+        """List alert channels
+
+        Args:
+            ids: channel ids
+            external_ids: channel external ids
+            parent_ids: channel parent ids
+            metadata: strict metadata filtering
+
+        Returns:
+            AlertChannelList: list of channels"""
+
+        filter = AlertChannelFilter(
+            external_ids=external_ids,
+            ids=ids,
+            parent_ids=parent_ids,
+            metadata=metadata,
+        ).dump(camel_case=True)
+
+        return self._list(method="POST", limit=limit, filter=filter)
+
+    def update(
+        self, items: Union[AlertChannel, AlertChannelUpdate, List[Union[AlertChannel, AlertChannelUpdate]]]
+    ) -> Union[AlertChannel, AlertChannelList]:
+        """Update alerting channels
+
+        Args:
+            items: Union[AlertChannel, AlertChannelUpdate, List[Union[AlertChannel, AlertChannelUpdate]]]: channel(s) to be updated
+
+        Returns:
+            Union[AlertChannel, AlertChannelList]: updated items"""
+        return self._update_multiple(items=items)
+
+    def delete(self, ids: List[int] = None, external_ids: List[str] = None) -> None:
+        self._delete_multiple(ids=ids, external_ids=external_ids, wrap_ids=True)
+
+
+class AlertSubscribersAPI(APIClient):
+    _RESOURCE_PATH = "/alerts/subscribers"
+    _LIST_CLASS = AlertSubscriberList
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    """Create subscribers
+
+        Args:
+            subscribers (Union[AlertSubscriber, List[AlertSubscriber]]): subscriber(s) to create
+
+        Returns:
+            Union[AlertSubscriber, AlertSubscriberList]: created subscribers(s)
+        """
+
+    def create(
+        self,
+        subscribers: Union[AlertSubscriber, List[AlertSubscriber]],
+    ) -> Union[AlertSubscriber, AlertSubscriberList]:
+        assert_type(subscribers, "subscribers", [AlertSubscriber, list])
+        return self._create_multiple(
+            items=subscribers,
+            resource_path=self._RESOURCE_PATH,
+        )
+
+
+class AlertSubscriptionsAPI(APIClient):
+    _RESOURCE_PATH = "/alerts/subscriptions"
+    _LIST_CLASS = AlertSubscriptionList
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    """Create subscriptions
+
+        Args:
+            subscriptions (Union[AlertSubscriptions, List[AlertSubscriptions]]): subscription(s) to create
+
+        Returns:
+            Union[AlertSubscriptions, AlertSubscriptionsList]: created subscription(s)
+        """
+
+    def create(
+        self,
+        subscriptions: Union[AlertSubscription, List[AlertSubscriptionList]],
+    ) -> Union[AlertSubscription, AlertSubscriptionList]:
+        assert_type(subscriptions, "subscriptions", [AlertSubscription, list])
+        return self._create_multiple(
+            items=subscriptions,
+            resource_path=self._RESOURCE_PATH,
+        )
+
+    """Delete subscriptions
+
+        Args:
+            ids: subscription ids to delete
+            external_ids: subscription external ids to delete
+
+        Returns:
+            None"""
+
+    def delete(self, cmds: List[AlertSubscriptionDelete]) -> None:
+        items_to_delete = [cmd.dump(camel_case=True) for cmd in cmds]
+
+        body = {"items": items_to_delete}
+        url = self._RESOURCE_PATH + "/delete"
+        self._post(url, json=body)
+
+
+class AlertsAPI(APIClient):
+    _RESOURCE_PATH = "/alerts/alerts"
+    _LIST_CLASS = AlertList
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channels = AlertChannelsAPI(*args, **kwargs)
+        self.subscribers = AlertSubscribersAPI(*args, **kwargs)
+        self.subscriptions = AlertSubscriptionsAPI(*args, **kwargs)
+
+    """Create alerts
+
+        Args:
+            alerts (Union[Alert, List[Alert]]): alert(s) to create
+
+        Returns:
+            Union[Alert, AlertList]: created alert(s)
+        """
+
+    def create(
+        self,
+        alerts: Union[Alert, List[Alert]],
+    ) -> Union[Alert, AlertList]:
+        assert_type(alerts, "alerts", [Alert, list])
+        return self._create_multiple(items=alerts, resource_path=self._RESOURCE_PATH)
+
+    def list(
+        self,
+        ids: List[int] = None,
+        external_ids: List[str] = None,
+        channel_ids: List[int] = None,
+        channel_external_ids: List[int] = None,
+        closed: bool = None,
+        start_time: str = None,
+        end_time: str = None,
+        limit=100,
+    ) -> AlertList:
+        """List alerts
+
+        Args:
+            ids: alert ids to filter on
+            external_ids: alert external_ids to filter on
+            channel_ids: alert channel_ids to filter on
+            channel_external_ids: alert channel_external_ids to filter on
+            closed: filter on whether alerts are closed or not
+            start_time: filter alerts based on timestamp
+            end_time: filter alerts based on timestamp
+
+
+        Returns:
+            AlertsList: list of alerts"""
+
+        filter = AlertFilter(
+            ids=ids,
+            external_ids=external_ids,
+            channel_ids=channel_ids,
+            channel_external_ids=channel_external_ids,
+            closed=closed,
+            start_time=start_time,
+            end_time=end_time,
+        ).dump(camel_case=True)
+
+        return self._list(method="POST", limit=limit, filter=filter)
+
+    def close(
+        self,
+        ids: List[int] = None,
+        external_ids: List[str] = None,
+    ) -> None:
+        """Close alerts
+
+        Args:
+            ids: alert(s) ids to close
+            external_ids: alert(s) external_ids to close
+
+
+        Returns:
+            None"""
+
+        all_ids = self._process_ids(ids, external_ids, wrap_ids=True)
+
+        self._post(self._RESOURCE_PATH + "/close", json={"items": all_ids}, headers={"cdf-version": "alpha"}).json()
+
+        return None
---
+++ tests/tests_integration/test_alerts/test_alerts.py
@@ -0,0 +1,219 @@
+import os
+import random
+from datetime import datetime, timezone
+from time import time
+from typing import Callable
+
+from pytest import fixture, mark
+
+from cognite.experimental import CogniteClient
+from cognite.experimental.data_classes.alerts import (
+    Alert,
+    AlertChannel,
+    AlertChannelUpdate,
+    AlertSubscriber,
+    AlertSubscription,
+    AlertSubscriptionDelete,
+)
+
+CURRENT_TS = datetime.now(timezone.utc)
+CURRENT_TS_INT = int(CURRENT_TS.timestamp())
+CURRENT_TS_STR = CURRENT_TS.strftime("%Y-%m-%dT%H:%M:%S")
+ALERTS_INT_TEST_EMAIL = f"ivan.polomanyi+{CURRENT_TS_INT}@cognite.com"
+
+
+@fixture(scope="class")
+def cognite_client() -> CogniteClient:
+    return CogniteClient(headers={"cdf-version": "alpha"})
+
+
+@fixture(scope="class")
+def base_alert() -> Callable[..., Alert]:
+    def create(channel_external_id: int) -> Alert:
+        return Alert(
+            external_id="test_" + CURRENT_TS_STR + str(random.random()),
+            channel_external_id=channel_external_id,
+            timestamp=CURRENT_TS_INT,
+            source="inttest",
+            level="INFO",
+            metadata={"test": "test"},
+            value="10 percent",
+        )
+
+    return create
+
+
+@fixture(scope="class")
+def base_channel() -> Callable[..., AlertChannel]:
+    def create() -> AlertChannel:
+        return AlertChannel(
+            external_id="test_" + CURRENT_TS_STR + "_" + str(random.random()),
+            name="test_channel_" + CURRENT_TS_STR,
+            description="integration test channel",
+            metadata={"test": "test"},
+        )
+
+    return create
+
+
+@fixture(scope="class")
+def base_subscriber() -> Callable[..., AlertSubscriber]:
+    def create() -> AlertSubscriber:
+        return AlertSubscriber(
+            external_id="test_" + CURRENT_TS_STR + str(random.random()),
+            metadata={"test": "test"},
+            email=ALERTS_INT_TEST_EMAIL,
+        )
+
+    return create
+
+
+@fixture(scope="class")
+def base_subscription() -> Callable[..., AlertSubscription]:
+    def create(channel_id: int, subscriber_id: int) -> AlertSubscription:
+        return AlertSubscription(
+            external_id="test_" + CURRENT_TS_STR + str(random.random()),
+            channel_id=channel_id,
+            subscriber_id=subscriber_id,
+            metadata={"test": "test"},
+        )
+
+    return create
+
+
+@mark.skipif(
+    os.environ.get("ENABLE_ALERTS_TESTS") == None, reason="Skipping alerts API tests due to service immaturity"
+)
+class TestAlertChannelsIntegration:
+    def test_create_1(self, cognite_client, base_channel):
+        res = cognite_client.alerts.channels.create(base_channel())
+        assert "test_" in res.external_id
+
+    def test_create_2(self, cognite_client, base_channel):
+        res = cognite_client.alerts.channels.create([base_channel(), base_channel()])
+
+        assert len(res) == 2
+
+    def test_list(self, cognite_client):
+        res = cognite_client.alerts.channels.list()
+
+        assert len(res) > 0
+
+    def test_update_by_alert_channel(self, cognite_client, base_channel):
+        created = cognite_client.alerts.channels.create([base_channel()])[0]
+
+        created.description = "updated description"
+        created.external_id = f"{created.external_id}_updated_ext_id"
+        created.metadata = {"a": "b"}
+
+        update_res = cognite_client.alerts.channels.update([created])
+
+        updated = cognite_client.alerts.channels.list(ids=[created.id])
+
+        assert update_res == updated
+
+        assert len(updated) == 1
+        assert updated[0].description == "updated description"
+        assert updated[0].metadata == {"a": "b"}
+        assert "updated_ext_id" in updated[0].external_id
+
+    def test_update_by_alert_channel_update(self, cognite_client, base_channel):
+        created = cognite_client.alerts.channels.create([base_channel()])[0]
+
+        update = (
+            AlertChannelUpdate(created.id)
+            .description.set("updated description")
+            .external_id.set(f"{created.external_id}_updated_ext_id")
+            .metadata.add({"a": "b"})
+        )
+
+        update_res = cognite_client.alerts.channels.update([update])
+
+        updated = cognite_client.alerts.channels.list(ids=[created.id])
+
+        assert update_res == updated
+
+        assert len(updated) == 1
+        assert updated[0].description == "updated description"
+        assert updated[0].metadata == {"a": "b", "test": "test"}
+        assert "updated_ext_id" in updated[0].external_id
+
+    def test_delete(self, cognite_client, base_channel):
+        created = cognite_client.alerts.channels.create(base_channel())
+
+        cognite_client.alerts.channels.delete(external_ids=[created.external_id])
+
+        res = cognite_client.alerts.channels.list(external_ids=[created.external_id])
+
+        assert len(res) == 0
+
+
+@mark.skipif(
+    os.environ.get("ENABLE_ALERTS_TESTS") == None, reason="Skipping alerts API tests due to service immaturity"
+)
+class TestAlertsIntegration:
+    def test_create_1(self, cognite_client, base_alert, base_channel):
+        channel = cognite_client.alerts.channels.create(base_channel())
+
+        res = cognite_client.alerts.create(base_alert(channel.external_id))
+
+        assert channel.external_id == res.channel_external_id
+
+    def test_create_2(self, cognite_client, base_alert, base_channel):
+        channel = cognite_client.alerts.channels.create(base_channel())
+
+        res = cognite_client.alerts.create([base_alert(channel.external_id), base_alert(channel.external_id)])
+
+        assert len(res) == 2
+
+    def test_close(self, cognite_client):
+        alerts = cognite_client.alerts.list(closed=False)
+
+        cognite_client.alerts.close(ids=[alerts[0].id])
+
+        check_closed = cognite_client.alerts.list(ids=[alerts[0].id])
+
+        assert check_closed[0].closed == True
+        assert len(check_closed) == 1
+        assert check_closed[0].id == alerts[0].id
+
+    def test_list(self, cognite_client):
+        res = cognite_client.alerts.list()
+
+        assert len(res) > 0
+
+
+@mark.skipif(
+    os.environ.get("ENABLE_ALERTS_TESTS") == None, reason="Skipping alerts API tests due to service immaturity"
+)
+class TestSubscribersIntegration:
+    def test_create_1(self, cognite_client, base_subscriber):
+        res = cognite_client.alerts.subscribers.create(base_subscriber())
+
+        assert ALERTS_INT_TEST_EMAIL == res.email
+
+
+@mark.skipif(
+    os.environ.get("ENABLE_ALERTS_TESTS") == None, reason="Skipping alerts API tests due to service immaturity"
+)
+class TestSubscriptionsIntegration:
+    def test_create_1(self, cognite_client, base_subscription, base_subscriber, base_channel):
+        channel = cognite_client.alerts.channels.create(base_channel())
+
+        subscriber = cognite_client.alerts.subscribers.create(base_subscriber())
+
+        res = cognite_client.alerts.subscriptions.create(base_subscription(channel.id, subscriber.id))
+
+        assert res.channel_id == channel.id
+
+    def test_delete_by_subscriber_and_channel(self, cognite_client, base_subscription, base_channel, base_subscriber):
+        channel = cognite_client.alerts.channels.create(base_channel())
+
+        subscriber = cognite_client.alerts.subscribers.create(base_subscriber())
+
+        item = cognite_client.alerts.subscriptions.create(base_subscription(channel.id, subscriber.id))
+
+        delete_item = AlertSubscriptionDelete(channel_id=item.channel_id, subscriber_id=item.subscriber_id)
+        res = cognite_client.alerts.subscriptions.delete([delete_item])
+
+        assert res == None
