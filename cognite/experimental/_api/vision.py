789a774d0fef0c8dae70ebef9202d6607368b509
Marvin <marvinreza@outlook.com>
2022-07-29 10:29:16 +0200

    Update cognite/experimental/_api/vision.py
    Co-authored-by: Milad Mobarhan <43236572+miladcognite@users.noreply.github.com>

--- cognite/experimental/_api/vision.py
+++ cognite/experimental/_api/vision.py
@@ -81,7 +81,7 @@ class VisionAPI(ContextAPI):
 
         Args:
             features (Union[Feature, List[Feature]]): The feature(s) to extract from the provided image files.
-            file_ids (List[int]): IDs of the image files to annotate. The images must already be uploaded in the same tenant.
+            file_ids (List[int]): IDs of the image files to annotate. The images must already be uploaded in the same CDF project.
             file_external_ids (List[str]): The external file ids of the image files to annotate
         Returns:
             AnnotateJobResults: Job result object which can be used to wait for and retrieve the annotation results.
