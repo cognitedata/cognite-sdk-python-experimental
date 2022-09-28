65fe4493204ff75ece24625e44a399e7753f147d
Tuan Nguyen <tuan.nguyen@cognite.com>
2022-09-28 21:01:30 +0200

    move geospatial rasters to main sdk

--- cognite/experimental/data_classes/geospatial.py
+++ cognite/experimental/data_classes/geospatial.py
@@ -4,22 +4,6 @@ from cognite.client import utils
 from cognite.client.data_classes._base import CogniteResource, CogniteResourceList
 
 
-class RasterMetadata:
-    """Raster metadata"""
-
-    def __init__(self, **properties):
-        for key in properties:
-            setattr(self, key, properties[key])
-
-    @classmethod
-    def _load(cls, resource: Dict, cognite_client=None):
-        instance = cls(cognite_client=cognite_client)
-        for key, value in resource.items():
-            snake_case_key = utils._auxiliary.to_snake_case(key)
-            setattr(instance, snake_case_key, value)
-        return instance
-
-
 class MvpMappingsDefinition(CogniteResource):
     """MVT mappings definition"""
 
